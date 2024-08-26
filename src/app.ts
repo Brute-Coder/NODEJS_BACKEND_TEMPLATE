67316ddbdd588c85a1d2bc8e67875877d48230bf
Soumya Sekhar Mahanti <soumyasekharmahanti@gmail.com>
2024-08-27 01:47:53 +0530

    feat: Added system and application health endpoints

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import router from "./router/apiRouter"
 import globalErrorHandler from "./middleware/globalErrorHandler"
 import httpError from "./util/httpError"
 import ResponseMessage from "./constant/responseMessage"
+import healthRouter from "./router/healthRouter"
 
 const app: Application = express()
 
@@ -11,16 +12,23 @@ const app: Application = express()
 app.use(express.json())
 app.use(express.static(path.join(__dirname, "../", "public")))
 
-//Routes
-app.use("/api/v1", router)
+//Main Router
+const mainRouter = express.Router()
+
+// Registering Sub Router
+mainRouter.use(router)
+mainRouter.use(healthRouter)
+
+// Registering Main Router
+app.use("/api/v1", mainRouter)
 
 // 404 Not Found Route
 app.use((req: Request, _: Response, next: NextFunction) => {
-    try {
-        throw new Error(ResponseMessage.NOT_FOUND("Route"))
-    } catch (error) {
-        httpError(next, error, req, 404)
-    }
+  try {
+    throw new Error(ResponseMessage.NOT_FOUND("Route"))
+  } catch (error) {
+    httpError(next, error, req, 404)
+  }
 })
 
 // GLOBAL ERROR HANDLER FallBack
